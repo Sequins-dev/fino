39052b7ae85faf79de63ee708c879212efa40a5d
Stephen Belanger <admin@stephenbelanger.com>
2026-09-03 05:03:50 +0800

    FIN-229: Add the VT terminal emulator (#65)

--- src/loader.rs
+++ src/loader.rs
@@ -305,6 +305,7 @@ static BUILTINS: &[BuiltinEntry] = &[
     source_builtin!("fino:tty/style", "tty/style"),
     source_builtin!("fino:tty/frame", "tty/frame"),
     source_builtin!("internal:tty/width", "internal/tty/width"),
+    source_builtin!("internal:tty/vt", "internal/tty/vt"),
     source_builtin!("fino:tty/tui", "tty/tui"),
     // net
     source_builtin!("internal:net/provider", "internal/net/provider"),
